41ea657499e39a6acc87e6ff44885f31de3e863e
Annais Molina <itsanndev@gmail.com>
2022-05-03 18:21:06 -0400

    Deleting actual server.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 
-console.log("Hello World");
+// (1) meeting node console
 bGround.log("Hello World");
+console.log("Hello World");
+
 module.exports = app;
