dcb82e0bec23b894e1b4d09fdb25ac88a930ff86
Annais Molina <itsanndev@gmail.com>
2022-05-03 10:29:36 -0400

    modifying app.js and server.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-const router = express.Router();
 var bGround = require('fcc-express-bground');
 
-// 1) Meet the node console
-bGround.log("Hello World");
+// (1) Meet the node console
+bGround.log('Hello World');
+
+module.exports = app;
