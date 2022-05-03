99988311cad4d7232732514887396be7bd2d8dce
Annais Molina <itsanndev@gmail.com>
2022-05-03 09:29:50 -0400

    Modyfing myApp.js

--- myApp.js
+++ myApp.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 
-// 1# Meet the node console
+// 1) Meet the node console
 bGround.log("Hello World");
